42eec74231fe3256e958f2551ae28b7517c8f6fb
ThomasITU <thwr@berkeley.edu>
2024-12-02 05:04:42 -0800

    Show grid layout overlaid on the image

--- src/centralNode/VideoToGraph.py
+++ src/centralNode/VideoToGraph.py
@@ -56,5 +56,43 @@ class VideoToGraph:
             cv.imshow('frame', gray)
             if cv.waitKey(1) == ord('q'):
                 break
+
+    def convert_image_to_graph(self, image):
+        corners = self.create_bounding_box(image)
+        grid, grid_width, grid_height = self.create_grid(corners)
+
+        
+        # Compute the affine transformation
+        matrix = self.compute_affine_transformation(corners, grid_width, grid_height)
+        overlay_image = image.copy()
+
+        print(f"pixel block size: {self.pixel_block_width_px}x{self.pixel_block_height_px}")
+        for row in range(grid.shape[0]):
+            for col in range(grid.shape[1]):
+                transformed_block = self.apply_affine_transform((col, row), matrix)
+        
+                (x, y,_) = transformed_block
+                # Create a centered bounding box for the grid cell
+                top_left_x = int(x - self.pixel_block_width_px / 2)
+                top_left_y = int(y - self.pixel_block_height_px / 2)
+
+                bottom_right_x = int(x + self.pixel_block_width_px / 2)
+                bottom_right_y = int(y + self.pixel_block_height_px / 2)
+
+                # Draw the rectangle
+                cv.rectangle(
+                    overlay_image,
+                    (top_left_x, top_left_y),
+                    (bottom_right_x, bottom_right_y),
+                    (0, 255, 0),  # Green color
+                    1  # Line thickness
+                )
+
+        # Visualize the graph
+        plt.figure(figsize=(8, 8))
+        plt.imshow(cv.cvtColor(overlay_image, cv.COLOR_BGR2RGB))  # Convert from BGR to RGB
+        plt.title("Grid Overlay on Image")
+        plt.axis('off')  # Hide axes
+        plt.show()
 # Detecting the objects
 
