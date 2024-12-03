7571c42ce53c743ac8f42a5c298bd69007a97aff
ThomasITU <thwr@berkeley.edu>
2024-12-03 01:10:01 -0800

    refactor graph logic into respective class add qrcode

--- src/centralNode/Graph.py
+++ src/centralNode/Graph.py
@@ -1,2 +1,144 @@
-# create graph
+import networkx as nx
+from util import UtilityFunctions as uf
+from typing import Optional
+import cv2 as cv
 
+class Graph(nx.Graph):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def set_node_positions(graph, matrix):
+        pos = {node: node for node in graph.nodes()}
+        nx.set_node_attributes(graph, pos, "pos")
+
+        real_pos = {node: uf.apply_affine_transform(node, matrix) for node in graph.nodes()}
+        nx.set_node_attributes(graph, real_pos, "real_pos")
+
+    def draw_nodes_overlay(graph, overlay_image):
+        for node in graph.nodes():
+            transformed_node = graph.nodes[node]["real_pos"]
+            near_obstacle = Graph.is_node_near_obstacle(graph, node)
+            color = uf.RED if near_obstacle else uf.GREEN
+            cv.circle(overlay_image, (int(transformed_node[0]), int(transformed_node[1])), radius=5, color=color, thickness=-1)
+        return overlay_image
+    
+    def draw_edges_overlay(graph, overlay_image):
+        for edge in graph.edges():
+            node1_x, node1_y, _ = graph.nodes[edge[0]]["real_pos"]
+            node2_x, node2_y, _ = graph.nodes[edge[1]]["real_pos"]
+            graph[edge[0]][edge[1]]['weight'] = uf.euclidean_distance((node1_x, node1_y), (node2_x, node2_y))
+            color = uf.RED if Graph.is_node_near_obstacle(graph, edge[0], edge[1]) else uf.BLUE
+            cv.line(overlay_image, 
+                    (int(node1_x), int(node1_y)), 
+                    (int(node2_x), int(node2_y)), 
+                    color=color, thickness=1)
+        return overlay_image
+
+    def is_node_near_obstacle(graph, node_a, node_b: Optional[any] = None):
+        if node_b is None:
+            return graph.nodes[node_a].get("is_near_obstacle")
+        return graph.nodes[node_a].get("is_near_obstacle") or graph.nodes[node_b].get("is_near_obstacle")
+
+    def add_diagonal_edges(grid_width, grid_height, graph):
+        for x, y in graph.nodes():
+            # Define the diagonal neighbors
+            diagonal_neighbors = [
+                (x - 1, y - 1),  # Northwest
+                (x - 1, y + 1),  # Northeast
+                (x + 1, y - 1),  # Southwest
+                (x + 1, y + 1)   # Southeast
+            ]
+
+            # Add edges to diagonal neighbors if they are within bounds
+            for nx_, ny_ in diagonal_neighbors:
+                if 0 <= nx_ < grid_width and 0 <= ny_ < grid_height:
+                    graph.add_edge((x, y), (nx_, ny_))
+    
+    def update_graph_weights_based_on_obstacles(graph):
+        for node in graph.nodes:
+            if graph.nodes[node].get("is_near_obstacle", False):
+                for neighbor in graph.neighbors(node):
+                    graph[node][neighbor]['weight'] = float('inf')
+                    graph[neighbor][node]['weight'] = float('inf') 
+
+    def adjust_graph_weights(graph):
+        for edge in graph.edges():
+            node_a, node_b = edge
+            if graph.nodes[node_a].get("is_near_obstacle", False) or graph.nodes[node_b].get("is_near_obstacle", False):
+                graph[node_a][node_b]['weight'] = float('inf')
+                graph[node_b][node_a]['weight'] = float('inf')
+            else:
+                node_a_x, node_a_y, _ = graph.nodes[node_a]["real_pos"]
+                node_b_x, node_b_y, _ = graph.nodes[node_b]["real_pos"]
+                graph[node_a][node_b]['weight'] = uf.euclidean_distance((node_a_x, node_a_y), (node_b_x, node_b_y))
+                graph[node_b][node_a]['weight'] = uf.euclidean_distance((node_a_x, node_a_y), (node_b_x, node_b_y))
+
+
+    def update_graph_based_on_obstacle(graph, contour, proximity_threshold):
+        for node in graph.nodes:
+            node_x, node_y,_ = graph.nodes[node]["real_pos"]
+            distance = abs(cv.pointPolygonTest(contour, (node_x,node_y), True)) 
+            if distance <= proximity_threshold:
+                graph.nodes[node]["is_near_obstacle"] = True
+            else:
+                graph.nodes[node].setdefault("is_near_obstacle", False)
+
+    def safe_astar_path(graph, start_node, goal_node, heuristic):
+        if not nx.has_path(graph, start_node, goal_node):
+            return None
+
+        path = nx.astar_path(graph, source=start_node, target=goal_node, weight="weight", heuristic=heuristic)
+        # Check if any edge in the path has infinite weight
+        for u, v in zip(path[:-1], path[1:]):
+            if graph[u][v].get('weight', None) == float('inf'):
+                print(f"No path exists: Infinite weight edge encountered between {u} and {v}.")
+                return None
+        return path
+
+    def print_path_weights(graph, path):
+        total_weight = 0
+        for i in range(len(path) - 1):
+            node1 = path[i]
+            node2 = path[i + 1]
+            weight = graph[node1][node2].get('weight', None)  # Access the weight of the edge
+            total_weight += weight if weight is not None else 0
+            print(f"Edge {node1} -> {node2}: weight = {weight}")
+        print(f"Total path weight: {total_weight}")
+
+    def heuristic(node, goal):
+        return uf.euclidean_distance(node, goal)       
+    
+    def draw_path_weights(image, graph, path):
+        overlay_image = image.copy()
+        for i in range(len(path) - 1):
+            node_a = path[i]
+            node_b = path[i + 1]
+            
+            pos_a = graph.nodes[node_a]['real_pos']
+            pos_b = graph.nodes[node_b]['real_pos']
+            recalc = uf.euclidean_distance(pos_a, pos_b)
+            if recalc != graph[node_a][node_b].get('weight', None):
+                print(f"Weight mismatch between nodes {node_a} and {node_b}. Recalculated: {recalc}, Stored: {graph[node_a][node_b].get('weight', None)}")
+            edge_weight = graph[node_a][node_b].get('weight', None)
+            cv.putText(overlay_image, str(edge_weight), (int(pos_a[0]), int(pos_a[1])), 
+                cv.FONT_HERSHEY_SIMPLEX, 0.8, uf.GREEN, 2)
+            
+        return overlay_image
+    
+    def draw_transformed_path(image, graph, path):
+        overlay_image = image.copy()
+        for i in range(len(path) - 1):
+            node_a = path[i]
+            node_b = path[i + 1]
+            
+            pos_a = graph.nodes[node_a]['real_pos']
+            pos_b = graph.nodes[node_b]['real_pos']
+
+            cv.line(overlay_image, (int(pos_a[0]), int(pos_a[1])), 
+                    (int(pos_b[0]), int(pos_b[1])), uf.BLUE, 2)  
+
+            cv.circle(overlay_image, (int(pos_a[0]), int(pos_a[1])), 
+                      5, uf.YELLOW, -1)  # Yellow circle
+
+        return overlay_image
--- src/centralNode/VideoToGraph.py
+++ src/centralNode/VideoToGraph.py
@@ -1,15 +1,13 @@
-import numpy as np
 import cv2 as cv
 import threading as th 
 import networkx as nx
-from typing import Optional
-from util import UtilityFunctions as uf
-import math
 
+from util import UtilityFunctions as uf
+from graph import Graph as gr
 
 def main():
     vg = VideoToGraph(75, 150)
-    image = cv.imread("img/maze_crop_modified_black.png")
+    image = cv.imread("img/maze_crop_corners_obstacle_qr_codes.png")
     vg.convert_image_to_graph(image)
 
 class VideoToGraph:
@@ -63,60 +61,43 @@ class VideoToGraph:
             cv.imshow('frame', gray)
             if cv.waitKey(1) == ord('q'):
                 break
-
+    
     def convert_image_to_graph(self, image):
-        corners = uf.create_bounding_box(image)
+        corners = uf.find_corners(image)
         self.set_dimensions(corners)
         graph = nx.grid_2d_graph(self.grid_width, self.grid_height)
-        
+        gr.add_diagonal_edges(self.grid_width, self.grid_height, graph)
         matrix = uf.compute_affine_transformation(corners, self.grid_width, self.grid_height)
+        gr.set_node_positions(graph, matrix)
+        gr.adjust_graph_weights(graph)        
 
-        pos = {node: node for node in graph.nodes()}
-        real_pos = {node: uf.apply_affine_transform(node, matrix) for node in graph.nodes()}
-        nx.set_node_attributes(graph, pos, "pos")
-        nx.set_node_attributes(graph, real_pos, "real_pos")
+        self.detect_static_obstacles(image, graph)
+        gr.update_graph_weights_based_on_obstacles(graph)
 
-        overlay_image = image.copy()
-        self.detect_black_areas(image, graph)
-
-        # Apply the affine transformation to each node in the graph
-        for node in graph.nodes():
-            transformed_node = graph.nodes[node]["real_pos"]
-            near_black = self.near_black_area(graph, node)
-            color = (0, 0, 255) if near_black else (0, 255, 0)
-            cv.circle(overlay_image, (int(transformed_node[0]), int(transformed_node[1])), radius=5, color=color, thickness=-1)
-
-        # Optionally, draw the edges between connected nodes
-        for edge in graph.edges():
-            node1_x, node1_y,_ = uf.apply_affine_transform(edge[0], matrix)
-            node2_x, node2_y,_ = uf.apply_affine_transform(edge[1], matrix)
-            graph[edge[0]][edge[1]]['weight'] = math.sqrt((node2_x - node1_x) ** 2 + (node2_y - node1_y) ** 2)
-            color =  (0, 0, 255) if self.near_black_area(graph, edge[0], edge[1]) else (255, 0, 0)
-            cv.line(overlay_image, 
-                    (int(node1_x), int(node1_y)), 
-                    (int(node2_x), int(node2_y)), 
-                    color=color, thickness=1)
+        overlay_image = self.draw_overlay(image, graph)
         uf.show_image(overlay_image)
-        self.penalize_near_black_areas(graph)
-        path = self.safe_astar_path(graph, (0, 0), (self.grid_width - 1, self.grid_height - 4), heuristic=self.heuristic)
+
+
+        start = (0, 0)
+        target = (self.grid_width - 5, self.grid_height - 15)
+
+        path = gr.safe_astar_path(graph, start, target, heuristic=gr.heuristic)
         if path:
-            overlay_image = self.draw_transformed_path(overlay_image, graph, path)
+            overlay_image = gr.draw_transformed_path(overlay_image, graph, path)
+            overlay_image = gr.draw_path_weights(overlay_image, graph, path)
+        objects = self.detect_objects(image)
+
+        for key in objects.keys():
+            pts = objects[key].astype(int)
+            cv.polylines(overlay_image, [pts], isClosed=True, color=uf.GREEN, thickness=2)
 
         uf.show_image(overlay_image)
-
-    def near_black_area(self, graph, node_a, node_b: Optional[any] = None):
-        if node_b is None:
-            return graph.nodes[node_a].get("is_near_black")
-        return graph.nodes[node_a].get("is_near_black") or graph.nodes[node_b].get("is_near_black")
-
-    def penalize_near_black_areas(self, graph):
-        for node in graph.nodes:
-            # Check if the current node is near a black area
-            if graph.nodes[node].get("is_near_black", False):
-                for neighbor in graph.neighbors(node):
-                    # Assign a very high weight to edges leading to this node
-                    graph[node][neighbor]['weight'] = float('inf')
-                    graph[neighbor][node]['weight'] = float('inf')  # For undirected graphs
+    
+    def draw_overlay(self, image, graph):
+        overlay_image = image.copy()
+        overlay_image = gr.draw_nodes_overlay(graph, overlay_image)
+        overlay_image = gr.draw_edges_overlay(graph, overlay_image)
+        return overlay_image
 
     def set_dimensions(self, corners, block_size_cm=4):
         # Compute grid dimensions based on the block size and image size
@@ -128,90 +109,39 @@ class VideoToGraph:
 
         self.grid_width = int(image_width_px / pixel_block_width_px)
         self.grid_height = int(image_height_px / pixel_block_height_px)         
- 
-    def detect_black_areas(self, image, graph, proximity_threshold=65):
+    
+    def detect_static_obstacles(self, image, graph, proximity_threshold=60):
+        overlay_image = image.copy()
+        hsv_image = cv.cvtColor(overlay_image, cv.COLOR_BGR2HSV)
+        pink_lower = [140, 50, 50]
+        pink_upper = [170, 255, 255]
+        contours = uf.find_contours(hsv_image, pink_lower, pink_upper)
 
-        image_copy = image.copy()
-        gray_scale =  cv.cvtColor(image_copy, cv.COLOR_BGRA2BGR) if image_copy.shape[-1] == 4 else cv.cvtColor(image_copy, cv.COLOR_BGR2GRAY)
-
-        # Threshold the image to isolate black areas
-        _, thresholded = cv.threshold(gray_scale, 5, 255, cv.THRESH_BINARY_INV)
-
-        # Find contours of the black areas
-        contours, _ = cv.findContours(thresholded, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        overlay_image = cv.cvtColor(gray_scale, cv.COLOR_GRAY2BGR)
-
-        # Filter contours based on area
-        min_contour_area = 1000 
-        filtered_contours = [cnt for cnt in contours if cv.contourArea(cnt) > min_contour_area]
+        MIN_CONTOUR_AREA = 3000
+        filtered_contours = [cnt for cnt in contours if cv.contourArea(cnt) > MIN_CONTOUR_AREA]
 
         for contour in filtered_contours:
-            cv.drawContours(overlay_image, [contour], -1, (0, 0, 255), 2)
-    
-            for node in graph.nodes:
-                node_x, node_y,_ = graph.nodes[node]["real_pos"]
-                distance = abs(cv.pointPolygonTest(contour, (node_x,node_y), True)) 
-                if distance <= proximity_threshold:
-                    graph.nodes[node]["is_near_black"] = True
-                else:
-                    graph.nodes[node].setdefault("is_near_black", False)
-                
-        uf.show_image(overlay_image)
+            cv.drawContours(overlay_image, [contour], -1, uf.RED, 2)
+            gr.update_graph_based_on_obstacle(graph, contour, proximity_threshold)
+            uf.show_image(overlay_image)
 
-    def draw_transformed_path(self, image, graph, path):
-        overlay_image = image.copy()
-        for i in range(len(path) - 1):
-            node_a = path[i]
-            node_b = path[i + 1]
-            
-            pos_a = graph.nodes[node_a]['real_pos']
-            pos_b = graph.nodes[node_b]['real_pos']
- 
-            cv.line(overlay_image, (int(pos_a[0]), int(pos_a[1])), 
-                    (int(pos_b[0]), int(pos_b[1])), (0, 255, 0), 2)  # Blue line
-            
-            cv.circle(overlay_image, (int(pos_a[0]), int(pos_a[1])), 5, (255,255,153), -1)  # Yellow circle
-            cv.circle(overlay_image, (int(pos_b[0]), int(pos_b[1])), 5, (255,255,153), -1)  
-
-        return overlay_image
-
-    def safe_astar_path(self,graph, start_node, goal_node, heuristic):
+    def detect_objects(self, image):
+        # Initialize the QR code detector
+        qcd = cv.QRCodeDetector()
+        objects = {}
         
-        # Ensure the nodes are reachable
-        if not nx.has_path(graph, start_node, goal_node):
-            return None
-
-        path = nx.astar_path(graph, source=start_node, target=goal_node, weight="weight", heuristic=heuristic)
-        # Check if any edge in the path has infinite weight
-        for u, v in zip(path[:-1], path[1:]):
-            if graph[u][v]['weight'] == float('inf'):
-                print(f"No path exists: Infinite weight edge encountered between {u} and {v}.")
-                return None
-        self.print_path_weights(graph, path)
-
-        return path
-
-    def print_path_weights(self,graph, path):
-        print("Edge weights along the A* path:")
-        total_weight = 0
-        for i in range(len(path) - 1):
-            node1 = path[i]
-            node2 = path[i + 1]
-            weight = graph[node1][node2].get('weight', None)  # Access the weight of the edge
-            total_weight += weight if weight is not None else 0
-            print(f"Edge {node1} -> {node2}: weight = {weight}")
-        print(f"Total path weight: {total_weight}")
-
-    def heuristic(self, node, goal):
-        (x1, y1) = node
-        (x2, y2) = goal
-        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
-    # Detecting the objects
-    def detect_objects():
-        pass
-        # qcd = cv.QRCodeDetector()
-        # outputs multiple QR codes
+        # Detect and decode QR codes
+        print("Detecting QR codes...")
+        retval, decoded_infos, points, _ = qcd.detectAndDecodeMulti(image)
+        
+        if retval:
+            print("QR codes detected.")
+            for i, decoded_info in enumerate(decoded_infos):
+                obj_type, obj_number = decoded_info.split(" ")
+                decoded_info = obj_type+"_"+obj_number
+                objects[decoded_info] = points[i]
+                print(f"QR Code {i+1}: {decoded_info}")
+        return objects
 
 if __name__ == "__main__":
     main()
